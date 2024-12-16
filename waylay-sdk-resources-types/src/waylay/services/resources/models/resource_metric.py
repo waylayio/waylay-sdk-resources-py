46f0839c960deef69b1ae25207d7bb6411561883
Thomas De Smedt <thomas@waylay.io>
2024-12-16 10:34:37 +0100

    fix: correct enum default issue

--- waylay-sdk-resources-types/src/waylay/services/resources/models/resource_metric.py
+++ waylay-sdk-resources-types/src/waylay/services/resources/models/resource_metric.py
@@ -22,7 +22,10 @@ from pydantic import (
 )
 from waylay.sdk.api._models import BaseModel as WaylayBaseModel
 
-from ..models.resource_metric_metric_type import ResourceMetricMetricType
+from ..models.resource_metric_metric_type import (
+    ResourceMetricMetricType,
+    ResourceMetricMetricTypeOneOf2,
+)
 
 
 class ResourceMetric(WaylayBaseModel):
@@ -40,7 +43,7 @@ class ResourceMetric(WaylayBaseModel):
         alias="valueChoices",
     )
     metric_type: ResourceMetricMetricType | None = Field(
-        default="gauge", alias="metricType"
+        default=ResourceMetricMetricTypeOneOf2.GAUGE, alias="metricType"
     )
     unit: StrictStr | None = Field(
         default=None,
